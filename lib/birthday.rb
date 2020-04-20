9b8fd4e8c334f96973fc6777c4c1bf83aebb04ab
Philippe Timothe <philippetimothe@gmail.com>
2020-04-20 19:21:05 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   birthday = ""
   birthday_kids.each do |kids_name, age|
     if <= 12
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    winner = "Happy Birthday #{kids_name}! You are now #{age} years old!"
 
   end
 end
